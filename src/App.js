1aad8b360f5bf23c5a7877f4726b2f14006bba35
akkySrivastava <akkyjaicar@gmail.com>
2021-05-05 10:59:42 +0530

    useState and useEffect hook tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Modal from "react-modal";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (modalOpen) {
+      setTimeout(() => setCount(count + 5), 500);
+    }
+  }, [count, modalOpen]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setModalOpen(true)}>Open Modal</button>
+      <Modal
+        isOpen={modalOpen}
+        shouldCloseOnOverlayClick={false}
+        onRequestClose={() => setModalOpen(false)}
+      >
+        <h1>useState Hook Tutorial</h1>
+        <h4>Count : {count}</h4>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        &nbsp;
+        <button onClick={() => setCount(count - 1)}>-</button>
+        &nbsp;
+        <button onClick={() => setModalOpen(false)}>Close</button>
+      </Modal>
     </div>
   );
 }
